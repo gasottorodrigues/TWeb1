2dcebf8d62685baa1d0a88fde9fd4b5660a2141a
igormoraes <moralizandoTv@gmail.com>
2018-03-28 17:23:10 -0300

    funcoes.php alterada

--- funcoes.php
+++ funcoes.php
@@ -1,1 +1,39 @@
- 
+<?php
+// Arquivo desenvolvido por: Igor Moraes
+// Funções gerais
+function menuNavegacao(){
+	echo '<div style="width:500px; margin: 0 auto;">Será menu</div>';
+}
+// Funções calculadora
+
+
+	function somar($x, $y){
+		$z = $x + $z;
+		return $z;
+	}
+	
+	function subtarir($x, $y){
+		$z = $x - $z;
+		return $z;
+	}
+
+	function dividir($x, $y){
+		$z = $x / $z;
+		return $z;
+	}
+
+	function multiplicar($x, $y){
+		$z = $x * $z;
+		return $z;
+	}
+	
+// Funções IMC
+
+	function (){
+		
+	}
+	
+	function (){
+		
+	}
+?> 
