9875a9b4d036c23b3bf1e0544bf0f130af662e73
Gabriel Borges de Moraes <gabrielborgesdemoraes@gmail.com>
2018-03-30 14:57:01 -0300

    geraIMC quase finalizado

--- gera_calculo_imc.php
+++ gera_calculo_imc.php
@@ -1,1 +1,70 @@
- 
+<?php
+$erro = 0;
+$erroId = 0;
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<title>Trabalho1</title>
+		<meta charset="UTF-8"/>
+		<link href="css/style.css" rel="stylesheet" type="text/css">	
+	</head>
+	<body>					
+		<?php
+		if(empty($_POST)){
+			$erro = 1;
+			$erroId = 1;
+		}
+		if(empty($_POST["usuarioIMC"])){
+			$erro = 1;
+			$erroId = 2;
+		}
+		
+		if(filter_input(INPUT_POST, "alturaIMC", FILTER_VALIDATE_FLOAT) or filter_input(INPUT_POST, "pesoIMC", FILTER_VALIDATE_FLOAT)){		
+			if(($_POST["alturaIMC"] > 5 or $_POST["alturaIMC"] < 0 ) or ($_POST["pesoIMC"] < 0 or $_POST["pesoIMC"] > 450)){
+				$erro = 1;
+				$erroId = 3;
+			}
+		}
+		else{
+			$erro = 1;
+			$erroId = 4;			
+		}
+		
+		if(file_exists("xml/usuarios.xml")){
+			$xml = simplexml_load_file("xml/usuarios.xml");
+			foreach ($xml->children() as $usuario){   
+				if($usuario->nome == $_POST["usuarioIMC"]){
+					$usuario = $_POST["usuarioIMC"];
+					break;
+				}
+				else{
+					$usuario = "Anônimo";
+				}
+			} 						
+		}
+		else{
+			$erro = 1;
+			$erroId = 5;			
+		}
+		
+		if($erro == 1){
+			echo "<script>console.log(".$erroId.");</script>";
+			echo'
+			<p>
+				Erro no envio do formulário, tente novamente mais tarde!					
+			</p>
+			<p>
+			Clique <a href="index.php">aqui</a> para voltar ao início
+			</p>
+			';
+		}
+		else{
+			$altura = $_POST["alturaIMC"];
+			$peso = $_POST["pesoIMC"];
+			
+			print_r($altura ." ".$peso ." ". $usuario);
+		}
+		?>				
+	</body>
+</html>  
--- imc.php
+++ imc.php
@@ -1,38 +1,46 @@
 <?php
-include("funcoes.php");
+/*include("funcoes.php");*/
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 	<head>
 		<title>Trabalho1</title>
 		<meta charset="UTF-8"/>
-		<link href="css/estilo.css" rel="stylesheet" type="text/css">	
+		<link href="css/style.css" rel="stylesheet" type="text/css">	
 	</head>
 	<body>
 		<form name="formCadastroIMC" class="form" action="gera_calculo_imc.php" method="post">
 			<div class="formGroup">
 				<label for="usuarioIMC">Usuário</label>
 				<select name="usuarioIMC" id="usuarioIMC">
+					<option selected>Anônimo</option>
 					<?php
-					if(file_exists("xml/"))	
+					if(file_exists("xml/usuarios.xml")){
+						$xml = simplexml_load_file("xml/usuarios.xml");
+						
+                        foreach ($xml->children() as $usuario){   
+							echo'<option>'.$usuario->nome.'</option>';
+                           
+                        }                     
+					}	
 					?>
 				</select>
 			</div>
 			
 			<div class="formGroup">
-				<label for="alturaIMC">Altura</label>
-				<input type="number" step="0.01" required="required" name="alturaImc" id="alturaIMC"/>
+				<label for="alturaIMC">Altura(m)</label>
+				<input type="number" step="0.01" required="required" min="0" max="5" name="alturaIMC" id="alturaIMC"/>
 			</div>
 			
 			<div class="formGroup">
-				<label for="alturaIMC">Peso</label>
-				<input type="text" required="required" name="pesoImc" id="pesoIMC"/>
+				<label for="alturaIMC">Peso(kg)</label>
+				<input type="text" required="required" min="0" max="450" name="pesoIMC" id="pesoIMC"/>
 			</div>
 			
 			<div class="formGroup">
 				<input type="submit" value="Enviar"/>
 				<input type="submit" value="Resetar"/>
 			</div>
-		<form>	
+		</form>	
 	</body>
 </html> 
