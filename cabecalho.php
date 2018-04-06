3231bdb62cf05c745da0ac8e21ef20c736b729a0
igormoraes <moralizandoTv@gmail.com>
2018-04-06 16:33:26 -0300

    Alteração funções e cabeçalho feitas por Igor

---
+++ cabecalho.php
@@ -0,0 +1,17 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--Arquivo feito por Igor Moraes -->
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Trabalho - IMC</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div id="header">
+	<a href="index.php" id="logo">Trabalho - IMC</a>
+	  <ul id="menu">
+		<li><a href="index.php"><span>Inicio</span></a></li>
+		<li><a href="form-cadastro-user.php"><span>Usuarios</span></a></li>
+		<li><a href="calculadora.php"><span>Calculadora</span></a></li>
+		<li><a href="imc.php"><span>IMC</span></a></li>
+	  </ul>
+	</div>
